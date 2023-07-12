9d68100fc35b3efb64392c76ee1006eb9f5a588b
AnnKruQA <128834696+AnnKruQA@users.noreply.github.com>
2023-07-12 13:58:53 +0200

    second_task_selectors

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,53 +2,23 @@ from pages.base_page import BasePage
 
 
 class Dashboard(BasePage):
-   1.
-   main_page_xpath = "//*[text()="
-   Main
-   page
-   "]"
-   2.
-   players_xpath = "//*[text()="
-   Players
-   "]"
-   3.
-   polski_xpath = "//*[text()="
-   Polski
-   "]"
-   4.
-   sign_out_button_xpath = "//*[text()="
-   Sign
-   out
-   "]"
-   5.
-   players_count_xpath = "//*[text()="
-   Players
-   count
-   "]"
-   6.
-   matches_count_xpath = "//*[text()="
-   Matches
-   count
-   "]"
-   7.
-   reports_count_xpath = "//*[text()="
-   Reports
-   count
-   "]"
-   8.
-   events_count_xpath = "//*[text()="
-   Events
-   count
-   "]"
-   9.
-   dev_team_contanct_button_xpath = "//*[text()="
-   Dev
-   team
-   contact
-   "]"
-   10.
-   add_player_button_xpath = "//*[text()="
-   Add
-   player
-   "]"
+   1. main_page_en_xpath = "//*[text()='Main page']"
+
+   2. players_en_xpath = "//*[text()='Players']"
+
+   3. polski_en_xpath = "//*[text()='Polski']"
+
+   4. sign_out_button_en_xpath = "//*[text()='Sign out']"
+
+   5. players_count_en_xpath = "//*[text()='Players count']"
+
+   6. matches_count_en_xpath = "//*[text()='Matches count']"
+
+   7. reports_count_en_xpath = "//*[text()='Reports count']"
+
+   8. events_count_en_xpath = "//*[text()='Events count']"
+
+   9. dev_team_contanct_button_en_xpath = "//*[text()='Dev team contact']"
+
+   10. add_player_button_en_xpath = "//*[text()='Add player']"
    pass
