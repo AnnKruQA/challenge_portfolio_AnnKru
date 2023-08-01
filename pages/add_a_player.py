437ad41550ac5354320c2c80d138ba92e3c1d14c
AnnKruQA <128834696+AnnKruQA@users.noreply.github.com>
2023-08-01 16:49:28 +0200

    third_task_assertion_tests

--- pages/add_a_player.py
+++ pages/add_a_player.py
@@ -2,4 +2,4 @@ from pages.base_page import BasePage
 
     class AddAPlayer(BasePage):
         expected_title = 'Add Player'
-        add_player_url = 'https://scouts-test.futbolkolektyw.pl/en/players/add'
+        dashboard_url = 'https://scouts-test.futbolkolektyw.pl/en/players/add'
